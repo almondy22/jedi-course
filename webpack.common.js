343b8e1faed99810eb64b6228a9ee7548317d4de
Gleb Klimov <klimovgleb@mail.ru>
2020-06-13 14:29:16 +0300

    Fifth commit (Prod & Dev Config)

--- webpack.common.js
+++ webpack.common.js
@@ -4,16 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
@@ -35,5 +29,5 @@ module.exports = {
         use: ["file-loader"]
       }
     ],
-  },
+  }
 };
