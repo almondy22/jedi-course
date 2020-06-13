ae50a40bee46a901fabd2947ffb1353afd203bdc
Gleb Klimov <klimovgleb@mail.ru>
2020-06-13 12:13:05 +0300

    Second commit (Assets setup)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require("path");
+const webpack = require('webpack')
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/"
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ],
   },
 };
